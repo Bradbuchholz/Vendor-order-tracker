ba00267b17dd273ec443b279b7b5e5071a5dc27f
Bradbuchholz <bradbuchholz@gmail.com>
2022-07-19 14:12:34 -0700

    second test failed

--- VendorAndOrderTracker/Models/Order.cs
+++ VendorAndOrderTracker/Models/Order.cs
@@ -4,6 +4,13 @@ namespace VendorAndOrderTracker.Models
 {
   public class Order
   {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public string Date { get; set; }
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> {};
 
+
+    }
   }
-}
