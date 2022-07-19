fb3f051789d3c49da72df453cc9c8445f6d51b5e
Bradbuchholz <bradbuchholz@gmail.com>
2022-07-19 13:47:57 -0700

    update controllers to remove errors, first order test fail successfully

--- VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -2,3 +2,27 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendorAndOrderTracker.Models;
 using System;
 using System.Collections.Generic;
+
+namespace VendorAndOrderTracker.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+    [TestMethod]
+    public void OrderConstructor_CreatesAnOrderObject_Order()
+    {
+      string title = "7 Bread";
+      string description = "The vendor wants bread this time";
+      int price = 15;
+      string date = "january 12";
+      Order newOrder = new Order(title, description, price, date);
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
+
+    
--- VendorAndOrderTracker/Controllers/HomeController.cs
+++ VendorAndOrderTracker/Controllers/HomeController.cs
@@ -1,14 +1,3 @@
 using Microsoft.AspNetCore.Mvc;
 using VendorAndOrderTracker.Models;
 
-namespace VendorAndOrderTracker.Controllers
-{
-  public class HomeController : Controller
-  {
-    [HttpGet("/")]
-    public ActionResult Index()
-    {
-      return View();
-    }
-  }
-}
