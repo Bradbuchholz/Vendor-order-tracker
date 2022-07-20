93388ea8ba3390a4042d865d5770690784c53693
Bradbuchholz <bradbuchholz@gmail.com>
2022-07-19 18:23:49 -0700

    passed test

--- VendorAndOrderTracker/Models/Order.cs
+++ VendorAndOrderTracker/Models/Order.cs
@@ -25,10 +25,13 @@ namespace VendorAndOrderTracker.Models
     {
       return _instances;
     }
-
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+      public static Order Find(int searchId)
+    {
+      return _instances[searchId - 1];
     }
   }
 }
